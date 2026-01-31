bdd4410c7b6eedff6ba577795573a0c3b6725bdc
Racquel Giner <racquel@Racquels-MacBook-Air.local>
2026-01-31 11:05:26 -0500

    added the home page and the MUI design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import "./App.css";
+import { Button, ButtonGroup } from "@mui/material";
+import MainLogo from "./assets/main-logo.png";
+import TopLogo from "./assets/white-logo.png";
 
 function App() {
   return (
-    <>
-      <div>The Contract Lounge</div>
-    </>
+    <div className="wrapper">
+      <header>
+        <div className="top-logo">
+          <img src={TopLogo} />
+        </div>
+        <div className="content">
+          <img src={MainLogo} className="main-logo" />
+          <p>
+            The contract lounge is a modern referral community for local
+            business pros - built around curated events, smart introductions,
+            and sponsor partners
+          </p>
+        </div>
+        <ButtonGroup variant="outlined" aria-label="Basic button group">
+          <Button> ABOUT US</Button>
+          <Button>SPONSORS</Button>
+          <Button>EVENTS</Button>
+          <Button>REGISTER</Button>
+        </ButtonGroup>
+      </header>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+// Optional: Customize your theme
+const theme = createTheme({
+  palette: {
+    mode: "light", // can also be 'dark'
+    primary: { main: "#ffffff" },
+    secondary: { main: "#9c27b0" },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
