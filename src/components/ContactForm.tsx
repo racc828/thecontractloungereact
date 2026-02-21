7cd2a7f3a261382981179a241c09a1ef31feaecd
Racquel Giner <racquel@Racquels-MacBook-Air.local>
2026-02-21 16:52:56 -0500

    added formspree

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,140 +3,151 @@ import { Button, TextField } from "@mui/material";
 const ContactForm = () => {
   return (
     <>
-      <TextField
-        id="name"
-        label="Name"
-        variant="filled"
-        margin="normal"
-        color="primary"
-        fullWidth
-        sx={{
-          "& .MuiFilledInput-root": {
-            color: "#ffffff",
-            // default underline
-            "&:before": {
-              borderBottomColor: "#ccc",
-            },
+      <form action="https://formspree.io/f/mdalwdyl" method="POST">
+        <TextField
+          id="name"
+          label="Name"
+          variant="filled"
+          margin="normal"
+          color="primary"
+          name="name"
+          fullWidth
+          sx={{
+            "& .MuiFilledInput-root": {
+              color: "#ffffff",
+              // default underline
+              "&:before": {
+                borderBottomColor: "#ccc",
+              },
 
-            // hover underline
-            "&:hover:not(.Mui-disabled, .Mui-error):before": {
-              borderBottomColor: "#fff",
-            },
+              // hover underline
+              "&:hover:not(.Mui-disabled, .Mui-error):before": {
+                borderBottomColor: "#fff",
+              },
 
-            // focused underline (THIS is when you click)
-            "&:after": {
-              borderBottomColor: "#6DB04B", // your color
+              // focused underline (THIS is when you click)
+              "&:after": {
+                borderBottomColor: "#6DB04B", // your color
+              },
             },
-          },
-          "& .MuiFilledInput-input": {
-            color: "#fff",
-          },
-          "& .MuiInputLabel-root": {
-            color: "#fff",
-          },
-        }}
-      />
-      <TextField
-        id="email"
-        label="Email"
-        variant="filled"
-        margin="normal"
-        fullWidth
-        sx={{
-          "& .MuiFilledInput-root": {
-            color: "#ffffff",
-            // default underline
-            "&:before": {
-              borderBottomColor: "#ccc",
+            "& .MuiFilledInput-input": {
+              color: "#fff",
             },
+            "& .MuiInputLabel-root": {
+              color: "#fff",
+            },
+          }}
+        />
+        <TextField
+          id="email"
+          label="Email"
+          variant="filled"
+          margin="normal"
+          fullWidth
+          name="email"
+          sx={{
+            "& .MuiFilledInput-root": {
+              color: "#ffffff",
+              // default underline
+              "&:before": {
+                borderBottomColor: "#ccc",
+              },
 
-            // hover underline
-            "&:hover:not(.Mui-disabled, .Mui-error):before": {
-              borderBottomColor: "#fff",
-            },
+              // hover underline
+              "&:hover:not(.Mui-disabled, .Mui-error):before": {
+                borderBottomColor: "#fff",
+              },
 
-            // focused underline (THIS is when you click)
-            "&:after": {
-              borderBottomColor: "#6DB04B", // your color
+              // focused underline (THIS is when you click)
+              "&:after": {
+                borderBottomColor: "#6DB04B", // your color
+              },
             },
-          },
-          "& .MuiFilledInput-input": {
-            color: "#fff",
-          },
-          "& .MuiInputLabel-root": {
-            color: "#fff",
-          },
-        }}
-      />
-      <TextField
-        id="number"
-        label="Number"
-        variant="filled"
-        margin="normal"
-        fullWidth
-        sx={{
-          "& .MuiFilledInput-root": {
-            color: "#ffffff",
-            // default underline
-            "&:before": {
-              borderBottomColor: "#ccc",
+            "& .MuiFilledInput-input": {
+              color: "#fff",
             },
+            "& .MuiInputLabel-root": {
+              color: "#fff",
+            },
+          }}
+        />
+        <TextField
+          id="number"
+          label="Number"
+          variant="filled"
+          margin="normal"
+          fullWidth
+          name="number"
+          sx={{
+            "& .MuiFilledInput-root": {
+              color: "#ffffff",
+              // default underline
+              "&:before": {
+                borderBottomColor: "#ccc",
+              },
 
-            // hover underline
-            "&:hover:not(.Mui-disabled, .Mui-error):before": {
-              borderBottomColor: "#fff",
-            },
+              // hover underline
+              "&:hover:not(.Mui-disabled, .Mui-error):before": {
+                borderBottomColor: "#fff",
+              },
 
-            // focused underline (THIS is when you click)
-            "&:after": {
-              borderBottomColor: "#6DB04B", // your color
+              // focused underline (THIS is when you click)
+              "&:after": {
+                borderBottomColor: "#6DB04B", // your color
+              },
             },
-          },
-          "& .MuiFilledInput-input": {
-            color: "#fff",
-          },
-          "& .MuiInputLabel-root": {
-            color: "#fff",
-          },
-        }}
-      />
-      <TextField
-        label="Message"
-        id="message"
-        variant="filled"
-        margin="normal"
-        fullWidth
-        multiline
-        rows={4}
-        sx={{
-          "& .MuiFilledInput-root": {
-            color: "#ffffff",
-            // default underline
-            "&:before": {
-              borderBottomColor: "#ccc",
+            "& .MuiFilledInput-input": {
+              color: "#fff",
             },
+            "& .MuiInputLabel-root": {
+              color: "#fff",
+            },
+          }}
+        />
+        <TextField
+          label="Message"
+          id="message"
+          variant="filled"
+          margin="normal"
+          fullWidth
+          name="message"
+          multiline
+          rows={4}
+          sx={{
+            "& .MuiFilledInput-root": {
+              color: "#ffffff",
+              // default underline
+              "&:before": {
+                borderBottomColor: "#ccc",
+              },
 
-            // hover underline
-            "&:hover:not(.Mui-disabled, .Mui-error):before": {
-              borderBottomColor: "#fff",
-            },
+              // hover underline
+              "&:hover:not(.Mui-disabled, .Mui-error):before": {
+                borderBottomColor: "#fff",
+              },
 
-            // focused underline (THIS is when you click)
-            "&:after": {
-              borderBottomColor: "#6DB04B", // your color
+              // focused underline (THIS is when you click)
+              "&:after": {
+                borderBottomColor: "#6DB04B", // your color
+              },
             },
-          },
-          "& .MuiFilledInput-input": {
-            color: "#fff",
-          },
-          "& .MuiInputLabel-root": {
-            color: "#fff",
-          },
-        }}
-      />
-      <Button variant="contained" color="primary" sx={{ mt: 2 }}>
-        Submit
-      </Button>
+            "& .MuiFilledInput-input": {
+              color: "#fff",
+            },
+            "& .MuiInputLabel-root": {
+              color: "#fff",
+            },
+          }}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+        >
+          Submit
+        </Button>
+      </form>
     </>
   );
 };
