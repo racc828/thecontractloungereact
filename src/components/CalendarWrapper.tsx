c3047acc4ba1c15db38db621e4a6646a8200a261
Racquel Giner <racquel@Racquels-MacBook-Air.local>
2026-03-05 15:21:14 -0500

    added new events

--- src/components/CalendarWrapper.tsx
+++ src/components/CalendarWrapper.tsx
@@ -36,6 +36,30 @@ const CalendarWrapper = () => {
             backgroundColor: "#6DB04B",
             textColor: "white",
           },
+          {
+            title: "Hammonton Happy Hour",
+            date: "2026-03-12",
+            url: "https://www.eventbrite.com/e/the-hammonton-happy-hour-tickets-1983737250517?utm_experiment=test_share_listing&aff=ebdsshios&sg=dd50440ef8e81515309783a500c30a0da3c5bf21047a9b956d0854f27d6bb3a3e9e55b750c3d3b456e5566bad7d3bc058617dd6a382241afb6d769a32c5cf7bea4af201c95b75e12963cefefaa26",
+            target: "_blank",
+            backgroundColor: "#6DB04B",
+            textColor: "white",
+          },
+          {
+            title: "Supreme Sponsored Event",
+            date: "2026-03-16",
+            url: "https://www.eventbrite.com/e/prepare-more-buyers-for-homeownership-credit-workshop-tickets-1984258845622?utm_experiment=test_share_listing&aff=ebdsshios&sg=dd50440ef8e81515309783a500c30a0da3c5bf21047a9b956d0854f27d6bb3a3e9e55b750c3d3b456e5566bad7d3bc058617dd6a382241afb6d769a32c5cf7bea4af201c95b75e12963cefefaa26",
+            target: "_blank",
+            backgroundColor: "#6DB04B",
+            textColor: "white",
+          },
+          {
+            title: "The Contract Lounge Event",
+            date: "2026-03-31",
+            url: "https://www.eventbrite.com/e/the-contract-lounge-a-business-networking-happy-hour-march-tickets-1983579505698?sg=dd50440ef8e81515309783a500c30a0da3c5bf21047a9b956d0854f27d6bb3a3e9e55b750c3d3b456e5566bad7d3bc058617dd6a382241afb6d769a32c5cf7bea4af201c95b75e12963cefefaa26&aff=ebdsshios",
+            target: "_blank",
+            backgroundColor: "#6DB04B",
+            textColor: "white",
+          },
         ]}
         eventClick={(info) => {
           info.jsEvent.preventDefault();
